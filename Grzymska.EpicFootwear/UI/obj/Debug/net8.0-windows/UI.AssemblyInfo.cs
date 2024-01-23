4c78f6c74ec0407bacc09e0876e3e9844995419c
Agnieszka Grzymska <agnieszkagrzymska1412@gmail.com>
2024-01-23 17:10:17 +0100

    Created the web app project

--- Grzymska.EpicFootwear/UI/obj/Debug/net8.0-windows/UI.AssemblyInfo.cs
+++ Grzymska.EpicFootwear/UI/obj/Debug/net8.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44edb646d802883a504d032aca76835c50e4b68e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
